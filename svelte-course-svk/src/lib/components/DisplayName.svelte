d5d793020fb9ca396113fdb0375e06019e987bfb
Bruno Silva <72350271+BrunoSilvaDF@users.noreply.github.com>
2026-01-26 18:24:07 -0300

    aula 14

---
+++ svelte-course-svk/src/lib/components/DisplayName.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	let firstName = $state('Bruno');
+	let lastName = $state('Silva');
+	let name = $derived.by(() => {
+		console.log('full name derived');
+		return `${firstName} ${lastName}`;
+	});
+	let username = $state('brunosilva');
+	let src = 'https://picsum.photos/200';
+	let bio = '<p>This is <strong>my</strong> bio</p>';
+
+	$effect(() => {
+		if (username || name) {
+			console.log('Username or full name changed');
+		}
+	});
+</script>
+
+<!-- <img {src} alt="Random Stuff" /> -->
+<input bind:value={username} />
+<input
+	value={firstName}
+	oninput={(e) => {
+		firstName = e.currentTarget.value;
+		name = 'blah';
+		console.log(name);
+	}}
+/>
+<input bind:value={lastName} />
+
+<h1>{username || name}</h1>
+
+<p>{@html bio}</p>
+
+<button onclick={() => console.log(name)}>Get fullname</button>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
---
+++ svelte-course-svk/src/lib/components/RandomNumber.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { untrack } from 'svelte';
+
+	let randomNumber = $state(Math.floor(Math.random() * 10));
+	let doubleRandomNumber = $derived(2 * randomNumber);
+	// let doubleRandomNumber = $state();
+
+	let history: number[] = $state([untrack(() => randomNumber)]);
+
+	// $effect(() => {
+	// 	doubleRandomNumber = randomNumber * 2;
+	// });
+
+	// $effect(() => {
+	// 	randomNumber;
+	// 	untrack(() => {
+	// 		history.push(randomNumber);
+	// 	});
+	// });
+</script>
+
+<h2>Random Number: {randomNumber}</h2>
+<h2>Double Random Number: {doubleRandomNumber}</h2>
+
+<p>{history.join(', ')}</p>
+
+<button
+	onclick={() => {
+		randomNumber = Math.floor(Math.random() * 10);
+		history = [...history, randomNumber];
+		console.log('randomNumber, doubleRandomNumber :>> ', randomNumber, doubleRandomNumber);
+	}}>Generate</button
+>
