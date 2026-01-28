2946136959818ac49930cb17b1531b429d548ea7
Bruno Silva <72350271+BrunoSilvaDF@users.noreply.github.com>
2026-01-28 18:55:37 -0300

    snipets

---
+++ svelte-course-svk/src/lib/components/Button.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		left?: Snippet;
+		right?: Snippet;
+		children: Snippet;
+	}
+	let { left, right, children }: Props = $props();
+</script>
+
+<button>
+	{#if left}
+		<div class="left">
+			{@render left()}
+		</div>
+	{/if}
+	{@render children()}
+	{#if right}
+		<div class="right">
+			{@render right()}
+		</div>
+	{/if}
+</button>
+
+<style>
+	button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 0.5rem 1rem;
+		background-color: #007bff;
+		color: white;
+		border: none;
+		border-radius: 4px;
+		cursor: pointer;
+		font-size: 1rem;
+	}
+
+	button:hover {
+		background-color: #0056b3;
+	}
+
+	.left {
+		margin-inline-end: 10px;
+	}
+
+	.right {
+		margin-inline-start: 10px;
+	}
+</style>
--- svelte-course-svk/src/routes/+page.svelte
+++ svelte-course-svk/src/routes/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	import Counter from '$lib/components/Counter.svelte';
 	import DisplayName from '$lib/components/DisplayName.svelte';
 	import RandomNumber from '$lib/components/RandomNumber.svelte';
+	import { AlarmClock, Search } from 'lucide-svelte';
 </script>
 
 <!-- <DisplayName /> -->
 
-<Counter />
+{#snippet left()}
+	<Search />
+{/snippet}
+
+<Button>
+	{#snippet left()}
+		<Search />
+	{/snippet}
+	Meu Botão com Ícone
+	{#snippet right()}
+		<AlarmClock />
+	{/snippet}
+</Button>
