2595d39c13ae6f412fbbdc87b839a7a8d2c7f17f
Bruno Silva <72350271+BrunoSilvaDF@users.noreply.github.com>
2026-03-16 19:02:12 -0300

    lesson 42

--- svelte-course-svk/src/routes/+page.svelte
+++ svelte-course-svk/src/routes/+page.svelte
@@ -1,37 +1,33 @@
 <script lang="ts">
-	import Notification from '$lib/components/Notification.svelte';
-	import generateNotifications from '$lib/utils/generate-notifications';
+	import Sheet from '$lib/components/sheet/Sheet.svelte';
 
-	let notifications = $state(generateNotifications(3));
+	let data = $state([
+		[
+			{ value: 'Item', bgColor: '#6aa84f', color: '#fff' },
+			{ value: 'Price', bgColor: '#6aa84f', color: '#fff' },
+			{ value: 'Quantity', bgColor: '#6aa84f', color: '#fff' },
+			{ value: 'Total', bgColor: '#51803d', color: '#fff' }
+		],
+		[{ value: 'Milk' }, { value: '3' }, { value: '2' }, { value: '=MULTIPLY(B2,C2)' }],
+		[{ value: 'Oats' }, { value: '4' }, { value: '1' }, { value: '=MULTIPLY(B3,C3)' }],
+		[{ value: 'Honey' }, { value: '5' }, { value: '2' }, { value: '=MULTIPLY(B4,C4)' }],
+		[{ value: 'Strawberries' }, { value: '2' }, { value: '2' }, { value: '=MULTIPLY(B5,C5)' }],
+		[
+			{ value: '', bgColor: '#b4a7d6' },
+			{ value: '', bgColor: '#b4a7d6' },
+			{ value: 'Total', bgColor: '#b4a7d6' },
+			{ value: '=SUM(D2,D3,D4,D5)', bgColor: '#8e7eb6' }
+		]
+	]);
 </script>
 
-<ul>
-	{#each notifications as notification, idx (notification.id)}
-		<li class="notification">
-			<Notification
-				{notification}
-				onremove={() => {
-					notifications.splice(idx, 1);
-				}}
-			/>
-		</li>
-	{:else}
-		<li class="empty">No notifications</li>
-	{/each}
-</ul>
+<Sheet {data} />
+
+<pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
 
 <style>
-	ul {
-		list-style: none;
-		padding: 10px;
-		margin: 0;
-		li {
-			margin-bottom: 10px;
-			padding: 10px;
-			background-color: #333;
-			border-radius: 5px;
-		}
-	}
 	:global {
 		body {
 			color: white;
---
+++ svelte-course-svk/src/routes/_page3 copy.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import Notification from '$lib/components/Notification.svelte';
+	import generateNotifications from '$lib/utils/generate-notifications';
+
+	let notifications = $state(generateNotifications(3));
+</script>
+
+<ul>
+	{#each notifications as notification, idx (notification.id)}
+		<li class="notification">
+			<Notification
+				{notification}
+				onremove={() => {
+					notifications.splice(idx, 1);
+				}}
+			/>
+		</li>
+	{:else}
+		<li class="empty">No notifications</li>
+	{/each}
+</ul>
+
+<style>
+	ul {
+		list-style: none;
+		padding: 10px;
+		margin: 0;
+		li {
+			margin-bottom: 10px;
+			padding: 10px;
+			background-color: #333;
+			border-radius: 5px;
+		}
+	}
+	:global {
+		body {
+			color: white;
+			font-size: 1.5em;
+			background-color: #222;
+		}
+	}
+</style>
